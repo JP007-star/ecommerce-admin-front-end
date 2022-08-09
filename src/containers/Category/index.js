7602de6cc86200863cc1265848d97e71dcd21052
unknown <justice_of_peace@yahoo.com>
2022-08-10 00:37:20 +0530

    category modal added

--- src/containers/Category/index.js
+++ src/containers/Category/index.js
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react'
-import { Col, Container, Row } from 'react-bootstrap'
-import { useDispatch, useSelector} from 'react-redux'
+import React, { useEffect,useState } from 'react'
+import { Col, Container, Row ,Modal,Button} from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
 import { getAllCategory } from '../../actions'
 import { Layout } from '../../components/Layout'
+import {Input} from '../../components/UI/Input'
 
 
 /**
@@ -11,34 +12,42 @@ import { Layout } from '../../components/Layout'
 **/
 
 export const Category = (props) => {
-    const category=useSelector(state =>state.category)
+    const category = useSelector(state => state.category)
+    const [categoryName,setCategoryName]=useState('')
+    const [parentCategoryId,setParentCategoryId]=useState('')
+    const [CategoryImage,setCategoryImage]=useState('')
 
-    const dispatch=useDispatch()
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const dispatch = useDispatch()
 
     useEffect(() => {
-       dispatch(getAllCategory())
-    },[]);
+        dispatch(getAllCategory())
+    }, []);
 
-    const renderCategories = (categories) =>{
-        let component=[]
-        for(let category of categories){
-            component.push( 
+    const renderCategories = (categories) => {
+        let component = []
+        for (let category of categories) {
+            component.push(
                 <li>
                     {category.name}
                     {category.children.length > 0 ? (<ul>{renderCategories(category.children)}</ul>) : null}
-                </li> 
+                </li>
             );
         }
         return component;
     }
-  return(
-       <Layout sidebar>
+    return (
+        <Layout sidebar>
             <Container>
                 <Row>
                     <Col md={12}>
-                        <div style={{display: 'flex', justifyContent: 'space-between' }}>
-                           <h3>Category</h3>
-                           <button className='btn btn-primary'><i className='fa fa-plus'></i></button>
+                        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                            <h3>Category</h3>
+                            <button className='btn btn-primary' onClick={handleShow}><i className='fa fa-plus'  ></i></button>
                         </div>
                     </Col>
                 </Row>
@@ -50,7 +59,27 @@ export const Category = (props) => {
                     </Col>
                 </Row>
             </Container>
-       </Layout>
-   )
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Add New Category</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Input
+                       value={categoryName}
+                       placeholder={`Enter a category name`}
+                       onChange={(e)=>setCategoryName(e.target.name)}
+                     />
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </Layout>
+    )
 
- }
+}
